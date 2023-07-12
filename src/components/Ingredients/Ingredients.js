36f2395394ff206a8fd7fadca7c7fbc31a30423a
Alberto Gonzalez <albertoglezb@hotmail.com>
2023-07-11 19:15:23 -0600

    Changes for the class: 'useEffect() & loading data'

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
@@ -7,6 +7,30 @@ import Search from './Search';
 function Ingredients() {
 
   const [ingredients, setIngredients] = useState([])
+
+  useEffect(() => {
+
+    fetch('https://react-hooks-update-65e04-default-rtdb.firebaseio.com/ingredients.json')
+      .then(response => response.json())
+      .then(responseData => {
+
+        const loadedIngredients = []
+        for (const key in responseData) {
+          loadedIngredients.push(
+            {
+              id: key,
+              title: responseData[key].title,
+              amount: responseData[key].amount
+            }
+          )
+        }
+
+        setIngredients(loadedIngredients)
+
+      })
+
+  }, [])
+
 
   const addIngredientHandler = ingredient => {
 
